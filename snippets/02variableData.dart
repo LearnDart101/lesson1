b74992fe8d65bceedeef0c5219cb5c2b61056ca3
Josh L. Minga <joshlminga@gmail.com>
2021-03-27 13:11:20 +0000

    The Lessons

---
+++ snippets/02variableData.dart
@@ -0,0 +1,19 @@
+void main() {
+
+  //Variables
+  String mainCharName = "Amos";
+  String mainFriendName = "Raymond";
+  String timeAge = "30";
+  
+  //Simple String
+  print("Long time ago, almost ${timeAge} years now in the days of rain times when music was just not entertainment but a message to the mass");
+  print("During those ages a young boy fell in love");
+  print("Do you know her? asked ${mainFriendName}, Yes my friend 'we study together' replied ${mainCharName}");
+  print("${mainCharName} really liked Jane, he was filled with Joy everytime he sees her during class, break hours or even after school");
+
+  // Overide the Variable value
+  mainFriendName = "Derrick";
+
+  print("${mainFriendName}: But ${mainCharName} you know Jane doesn't know that?");
+
+}
