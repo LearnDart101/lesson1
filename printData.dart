9226f22da245739f354d77b8bf5d3f6220d23e0e
joshlminga <joshlminga@gmail.com>
2021-03-27 15:46:12 +0300

    Files

---
+++ printData.dart
@@ -0,0 +1,10 @@
+void main() {
+  print("    /|");
+  print("   /_|");
+  print("  /__|");
+  print(" /___|");
+  print("/____|");
+
+  print("*************************");
+  print("There was once a man called Alpha, he was the best.");
+}
